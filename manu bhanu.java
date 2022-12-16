f3063613cbf62876642e0d3b9bbfb3777c8c7a2b
Manugowda08 <117275860+manugowda08@users.noreply.github.com>
2022-12-16 11:29:41 +0530

    Update manu bhanu.java

--- manu bhanu.java
+++ manu bhanu.java
@@ -8,7 +8,7 @@ class Stars {
             for(int k=1;k<=2*i-2;++k){
                 System.out.print(" ");
             }
-            for(int j=1;j<=5-i-1;++j){
+            for(int j=1;j<=5-i+1;++j){
                 System.out.print("*");
 
             }
@@ -18,11 +18,10 @@ class Stars {
             for(int j=1;j<=i+1;++j){
                 System.out.print("*");
             }
-            for(int k=1;k<=4-i+1;++k){
+            for(int k=1;k<=2*(4-i+1);++k{
                 System.out.print(" ");
             }
-            for(int k=1;k<=4-i+1;++k){
-                System.out.print(" ");
+            
             }
             for(int j=1;j<=i;++j){
                 System.out.print("*");
